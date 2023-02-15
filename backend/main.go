d40d3e898cd8e88c4f09e28f995a6922b17c3da8
gstrainovic <g.strainovic@gmail.com>
2023-02-15 18:46:09 +0100

    db bak

--- backend/main.go
+++ backend/main.go
@@ -5,19 +5,19 @@ import (
 	"log"
 
 	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/core"
+	// "github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
 	app := pocketbase.New()
 
-	app.OnRecordBeforeCreateRequest().Add(func(e *core.RecordCreateEvent) error {
-		// overwrite the newly submitted "posts" record status to pending
-		if e.Record.Collection().Name == "posts" {
-			e.Record.Set("status", "pending1235")
-		}
-		return nil
-	})
+	// app.OnRecordBeforeCreateRequest().Add(func(e *core.RecordCreateEvent) error {
+	// 	// overwrite the newly submitted "posts" record status to pending
+	// 	if e.Record.Collection().Name == "posts" {
+	// 		e.Record.Set("status", "pending1235")
+	// 	}
+	// 	return nil
+	// })
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
